46b9d16e1f7b2c7953975e2555138f28adf9a445
Rahaf Egbaria <95097302+RahafEgbaria@users.noreply.github.com>
2024-12-08 11:51:26 +0200

    Update display_simple.ino

--- Unit Tests/display_simple.ino
+++ Unit Tests/display_simple.ino
@@ -19,7 +19,7 @@
 #define SCREEN_HEIGHT 64     // OLED display height, in pixels
 #define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C  // Change if required
-#define ROTATION 0           // Rotates text on OLED 1=90 degrees, 2=180 degrees
+#define ROTATION 2           // Rotates text on OLED 1=90 degrees, 2=180 degrees
 
 // Define display object
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -43,7 +43,7 @@ void setup() {
 
   // Display settings
   display.clearDisplay();
-  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
   display.setRotation(ROTATION);      // Set screen rotation
