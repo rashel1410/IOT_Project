85af7cb497d2d3340644fd6e997001b9bf1f9a2a
Yaniv Valdman <yanivvaldman@campus.technion.ac.il>
2024-12-24 21:01:25 +0200

    .

--- server/main.py
+++ server/main.py
@@ -1,7 +1,27 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import MsgPayload
 
 app = FastAPI()
+
+# Configure CORS
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+    "http://172.20.10.2:8045",  # Add your local IP address here
+    "http://172.20.10.1:8045",  # Add your local IP address here
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 messages_list: dict[int, MsgPayload] = {}
 
 
@@ -31,3 +51,8 @@ def add_msg(msg_name: str) -> dict[str, MsgPayload]:
 @app.get("/messages")
 def message_items() -> dict[str, dict[int, MsgPayload]]:
     return {"messages:": messages_list}
+
+
+if __name__ == "__main__":
+    print("Starting server...")
+    uvicorn.run(app, host="0.0.0.0", port=8045)
