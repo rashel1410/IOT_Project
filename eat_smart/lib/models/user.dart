7b1d6f94caf5e762db279f448ffc0367d1e64904
Rashel Strigevsky <rashstar14@gmail.com>
2024-12-26 14:43:57 +0200

    Mockup screen and server connection are done

--- eat_smart/lib/models/user.dart
+++ eat_smart/lib/models/user.dart
@@ -1,34 +1,94 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'food_nutrients.dart';
 import 'food_item.dart';
 
 class User {
   final String name;
   final List<FoodItem> foods;
-  User({required this.name, required this.foods});
+  User({this.name = '',this.foods = const []});
 
   String getUserName() {
-    return ''; //name;
+    return this.name; //name;
   }
 
-  Future<List<FoodItem>> getUserFoods() async{
+
+  Future<List<FoodItem>> getUserFoodsMockData() async {
+    // Mock data
+    return [
+      FoodItem(
+        name: 'Apple',
+        nutrients: [
+          FoodNutrients(
+            nutrientId: 1,
+            nutrientName: 'Vitamin C',
+            nutrientNumber: 'C',
+            unitName: 'mg',
+            value: 5.0,
+          ),
+          FoodNutrients(
+            nutrientId: 2,
+            nutrientName: 'Fiber',
+            nutrientNumber: 'F',
+            unitName: 'g',
+            value: 2.0,
+          ),
+        ],
+      ),
+      FoodItem(
+        name: 'Banana',
+        nutrients: [
+          FoodNutrients(
+            nutrientId: 3,
+            nutrientName: 'Potassium',
+            nutrientNumber: 'K',
+            unitName: 'mg',
+            value: 10.0,
+          ),
+          FoodNutrients(
+            nutrientId: 4,
+            nutrientName: 'Vitamin B6',
+            nutrientNumber: 'B6',
+            unitName: 'mg',
+            value: 1.0,
+          ),
+        ],
+      ),
+      FoodItem(
+        name: 'Carrot',
+        nutrients: [
+          FoodNutrients(
+            nutrientId: 5,
+            nutrientName: 'Vitamin A',
+            nutrientNumber: 'A',
+            unitName: 'IU',
+            value: 100.0,
+          ),
+          FoodNutrients(
+            nutrientId: 6,
+            nutrientName: 'Fiber',
+            nutrientNumber: 'F',
+            unitName: 'g',
+            value: 3.0,
+          ),
+        ],
+      ),
+    ];
+  }
+
+  Future<List<FoodItem>> getUserFoods() async {
+
     var backendIP = '10.100.102.19';
     var port = '8045';
-    print("1111");
-    var res = await http.get(
-        Uri.parse('http://' + backendIP + ':' + port)
-    );
-    print("2222");
+    var res = await http.get(Uri.parse('http://$backendIP:$port'));
+    print("Waiting for response from server...");
 
     if (res.statusCode == 200) {
-      print("3333");
+      print("Response from server:");
       print(res.body);
-      List<dynamic> body = jsonDecode(res.body);
-      List<FoodItem> foodItems = body.map((dynamic item) => FoodItem.fromJson(item)).toList();
-      return foodItems;
     } else {
-      throw Exception('Failed to load food items');
+      print('Request failed with status: ${res.statusCode}.');
     }
+    return this.getUserFoodsMockData();
   }
 }
-
--- eat_smart/lib/screens/user_nutri_info_screen.dart
+++ eat_smart/lib/screens/user_nutri_info_screen.dart
@@ -1,29 +1,69 @@
 import 'package:flutter/material.dart';
 import '../models/user.dart';
-import '../models/food_nutrients.dart';
+import '../models/food_item.dart';
 
-class UserNutriInfoScreen extends StatelessWidget {
-  final User user;
+class UserFoodScreen extends StatefulWidget {
+  const UserFoodScreen({Key? key}) : super(key: key);
 
-  UserNutriInfoScreen({required this.user});
+  @override
+  _UserFoodScreenState createState() => _UserFoodScreenState();
+}
+
+class _UserFoodScreenState extends State<UserFoodScreen> {
+  late Future<List<FoodItem>> items;
+  late User user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = User(name: 'Dani', foods: []);
+    items = user.getUserFoods();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(user.getUserName()),
+        toolbarHeight: 100.0,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30.0, bottom: 20.0), // Add padding to the top
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Hey ${user.getUserName()}!'),
+              SizedBox(height: 7), // Add vertical space
+              Text('Here\'s what you ate today:'),
+            ],
+          ),
+        ),
       ),
-      body: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (context, index) {
-          final food = 3;
-          return ListTile(
-            title: Text('food.name'),
-            subtitle: Text('Calories: , Protein: g, Carbs: g, Fat: g'),
-          );
+
+      body: FutureBuilder<List<FoodItem>>(
+        future: items,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No food items found'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final foodItem = snapshot.data![index];
+                return ListTile(
+                  title: Text(foodItem.name),
+                  subtitle: Text(
+                      foodItem.nutrients.map(
+                              (nutrient) => '${nutrient.nutrientName}: ${nutrient.value} ${nutrient.unitName}').join(', ')
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
   }
 }
-
